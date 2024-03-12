1452ec9289aa4e2595d8f2e7f8cc884864b0e2c7
Estagiario 3 <estagiario3@movtech.com.br>
2024-03-12 15:21:00 -0300

    commit

--- script.js
+++ script.js
@@ -9,18 +9,24 @@ function addTask() {
         my_element = `
         <div id="container-${index}" class="itens">
         
-            <div style="display: flex; justify-content: center; align-items: center;"  id=text-${index}>${taskText}</div>
+            <div style="display: flex; justify-content: center; align-items: center; font-size: 20px; margin-bottom: -15px"  id=text-${index}>${taskText}</div>
             
             
             <div style="display:flex; justify-content: space-between; gap: 8px; padding-bottom: 5px; padding-top:10px"> 
+                <div class="button-itens">
                 <div onclick=concluir(${index})><i class="bi bi-check-all"></i></div>
+                </div>
+                <div class="button-itens vermelho">
                 <div onclick=excluir(${index})><i class="bi bi-trash-fill"></i></div>
+                </div>
+                <div class="button-itens azul">
                 <div onclick=editar(${index})><i class="bi bi-pencil-square"></i></div>
+                </div>
             </div>
 
             
         </div>
-        <hr>
+        <hr id="hr-${index}">
         
         `
         div.innerHTML = my_element
@@ -33,18 +39,17 @@ function addTask() {
 
 function excluir(index) {
     let container = document.querySelector(`#container-${index}`)
-    container.style = 'display: none'
+    let hr = document.querySelector(`#hr-${index}`);
+    container.style = 'display: none';
+    hr.remove();
 }
 function editar(index) {
-    let input = document.querySelector("#new-task-input");
     let text = document.querySelector(`#text-${index}`)
-    console.log(text)
 
-    var newText = prompt("Editar tarefa:", input.value);
-    if (newText == null) {
-        input.value = newText;
+    var newText = prompt("Editar tarefa:", text.textContent);
+    if (newText !== null && newText.trim() !== "") { 
+        text.textContent = newText.trim(); 
     }
-    text.innerHTML = newText;
 }
 
 function concluir(index) {
