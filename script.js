008186c283f76f8bf866161454efe8b554cdab77
JOÃO VITOR CIRICO <joao.cirico@catolicasc.edu.br>
2024-03-11 21:27:04 -0300

    Second commit

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
-const inputElement = document.querySelector("new-task-input");
-const addTaskbutton = document.querySelector(".new-task-button");
+let index = 0
+function addTask() {
+    let input = document.querySelector("#new-task-input");
+    let taskText = input.value.trim();
 
-const validateInput = () => inputElement.value.trim().length > 0;
+    if (taskText !== "") {
+        let div = document.createElement("div");
+        let container = document.querySelector("#tasks-container");
+        my_element = `
+        <div id="container-${index}" class="itens">
+        
+            <div id=text-${index}>${taskText}</div>
+            
+            <div style="display:flex; justify-content: space-between"> 
+                <div onclick=concluir(${index})><i class="bi bi-check-all"></i></div>
+                <div onclick=excluir(${index})><i class="bi bi-trash-fill"></i></div>
+                <div onclick=editar(${index})><i class="bi bi-pencil-square"></i></div>
+            </div>
+        </div>
 
-const handleAddTask = () =>{
-    const inputIsValid = validateInput();
-
-    console.log(inputIsValid)
-
-    if(!inputIsValid){
-        return inputElement.classList.add('error');
+        
+        `
+        div.innerHTML = my_element
+        container.appendChild(div)
+        input.value = ''
+        index++
     }
-};
 
-const handleInputChange = () =>{
-    const inputIsValid = validateInput();
+}
 
-    if (inputIsValid){
-        return inputElement.classList.remove("error");
+function excluir(index) {
+    let container = document.querySelector(`#container-${index}`)
+    container.style = 'display: none'
+}
+function editar(index) {
+    let input = document.querySelector("#new-task-input");
+    let text = document.querySelector(`#text-${index}`)
+    console.log(text)
+
+    var newText = prompt("Editar tarefa:", input.value);
+    if (newText == null) {
+        input.value = newText;
+    }
+    text.innerHTML = newText;
+}
+
+function concluir(index) {
+    let container = document.querySelector(`#text-${index}`)
+    if (container.className == 'completed') {
+        container.className = ''
+    } else {
+        container.className = 'completed'
     }
 }
-addTaskbutton.addEventListener("click", () => handleAddTask());
-
-inputElement.addEventListener("change", () => handleInputChange())
-
-
